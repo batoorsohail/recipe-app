c8f58ef214c78650d3d3ff671206781f0be46654
unknown <sohailshahidi1435@gmail.com>
2023-06-05 20:36:28 +0430

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  devise_for :users
+
+  resources :users, only: [:index] do
+    resources :foods, only: [:index] do
+      resources :recpie_foods, only: [:index]
+    end
+    resources :recipes, only: [:index, :show] do
+      resources :recpie_foods, only: [:index]
+    end
+  end
+  
+  root "users#index"
+
 end
