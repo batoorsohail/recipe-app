a74baea3920b16890b2a2b395a6b493b169de12e
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-06-05 15:35:25 +0000

    Run database migrations

--- db/migrate/20230605145930_create_foods.rb
+++ db/migrate/20230605145930_create_foods.rb
@@ -1,7 +1,7 @@
 class CreateFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :foods do |t|
-      t.references :user_id, null: false, foreign_key: { to_table: :users }
+      t.references :user, null: false, foreign_key: { to_table: :users }
       t.string :name
       t.string :measurement_unit
       t.integer :price
--- db/migrate/20230605152232_create_recipe_foods.rb
+++ db/migrate/20230605152232_create_recipe_foods.rb
@@ -2,8 +2,8 @@ class CreateRecipeFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :recipe_foods do |t|
       t.integer :quantity
-      t.references :food_id, null: false, foreign_key: {to_table: :foods}
-      t.references :recipe_id, null: false, foreign_key: {to_table: :recipes}
+      t.references :food, null: false, foreign_key: {to_table: :foods}
+      t.references :recipe, null: false, foreign_key: {to_table: :recipes}
 
       t.timestamps
     end
