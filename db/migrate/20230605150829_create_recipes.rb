d46a4736dfa7f3b8de57f2957e7ec591d44c6d03
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-06-05 15:29:38 +0000

    Generate recipe food table and add associations

--- db/migrate/20230605150829_create_recipes.rb
+++ db/migrate/20230605150829_create_recipes.rb
@@ -1,7 +1,7 @@
 class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
-      t.references :user_id, null: false, foreign_key: { to_table: 'users' }
+      t.references :user_id, null: false, foreign_key: { to_table: :users }
       t.string :name
       t.integer :preparation_time
       t.integer :cooking_time
