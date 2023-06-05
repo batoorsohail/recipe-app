9debe2fe961286d6e046978acca1cde3f0579189
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-06-05 15:21:41 +0000

    Associations for recipe and food

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,3 @@
-class Food < ApplicationRecord
+class Recipe < ApplicationRecord
     belongs_to :name, class_name: "User", foreign_key: :user_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-    has_many :foods, class_name: "food", foreign_key: :user_id, dependent: :destroy
+    has_many :foods, foreign_key: :user_id, dependent: :destroy
+    has_many :recipes, foreign_key: :user_id, dependent: :destroy
 end
