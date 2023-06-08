9f3dc657f7319a9e310cb9911f0c4ffb25f90401
unknown <sohailshahidi1435@gmail.com>
2023-06-08 10:14:54 +0430

    Add RecipeFoods controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'recipe_foods/new'
+  get 'recipe_foods/edit'
   get 'users/index'
   get 'recipes/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
---
+++ test/controllers/recipe_foods_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class RecipeFoodsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get recipe_foods_new_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get recipe_foods_edit_url
+    assert_response :success
+  end
+end
