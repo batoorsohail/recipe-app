488444f9ea62f306242b7f681ffdbe3e85fc4c11
unknown <sohailshahidi1435@gmail.com>
2023-06-10 14:39:47 +0430

    Update files

--- spec/integration/recipe_spec.rb
+++ spec/integration/recipe_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe 'recipes/index', type: :feature do
   describe 'after log in' do
     before(:each) do
-      @user = User.create(name: 'Jonas', email: 'jonasnw@gmail.com', password: 'jonas123', confirmed_at: Time.now)
+      @user = User.create(name: 'Test', email: 'test@gmail.com', password: 'Test123', confirmed_at: Time.now)
       @recipe1 = Recipe.create(user_id: @user.id, name: 'Burger', preparation_time: 20, cooking_time: 50, description: 'Home style american burger', public: true)
       @recipe2 = Recipe.create(user_id: @user.id, name: 'Pizza', preparation_time: 10, cooking_time: 30, description: 'Classic Italian pizza', public: false)
 
       visit 'users/sign_in'
-      fill_in 'Email', with: 'jonasnw@gmail.com'
-      fill_in 'Password', with: 'jonas123'
+      fill_in 'Email', with: 'test@gmail.com'
+      fill_in 'Password', with: 'Test123'
       click_on 'Log in'
     end
 
